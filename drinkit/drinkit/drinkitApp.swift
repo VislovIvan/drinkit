dcfa88f041da100a91b1fbc01c76cb868721a096
Vislov Ivan <74829720+VislovIvan@users.noreply.github.com>
2023-11-15 14:48:46 +0100

    Start Project Drinkit

---
+++ drinkit/drinkit/drinkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  drinkitApp.swift
+//  drinkit
+//
+//  Created by Ivan Vislov on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct drinkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
